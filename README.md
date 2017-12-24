71b635d10648dd2d97316862a1667cae624c7e82
treemmett <treemmett@gmail.com>
2017-12-23 22:44:02 -0700

    Adds new item API

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### Get All Items
 
-  Returns JSON of all hardware in the database.
+  Returns JSON of all items in the inventory.
 
 * **URL**
 
@@ -17,11 +17,52 @@
 * **Success Response**
 
   * **Code:** 200<br/>
-    **Body:** `[ {
+    **Body:** `[{
       id: 1,
       asset: 05261,
       model: "HP 850 G3",
       serial: "00000000",
       status: "working",
       location: "storage"
-    } ]`
+    }]`
+
+### Add New Items
+
+  Adds a new item to the inventory.
+
+* **URL**
+
+  /api/inventory
+
+* **Method**
+
+  `POST`
+
+* **Data Params**
+
+  **Required:**
+
+  * `asset` Asset tag
+  * `model` Model number
+  * `serial` Serial number
+
+  **Optional:**
+
+  * `location` Physical location _(default - storage)_
+  * `os` Operating system
+  * `status` Working condition _(default - working)_
+
+* **Success Response**
+
+  * **Code:** 200<br/>
+  **Body:** `{success: true}`
+
+* **Error Response**
+
+  * **Code:** 422<br/>
+  **Body:** `{error: 'Missing required data'}`
+
+  OR
+
+  * **Code:** 500<br/>
+  **Body:** `{error: 'Database query failed'}`
--- api/routes/inventory/index.js
+++ api/routes/inventory/index.js
@@ -4,14 +4,47 @@ const db = require(__root + 'database');
 inventory.get('/', (req, res, next) => {
   //Fetch information from database
   db.any('SELECT asset, model, serial, os, status, location FROM inventory')
+
   .then(result => {
     res.json(result);
     return next();
+  })
 
-  }).catch(err => {
+  .catch(err => {
     res.status(500).json({error: 'Server Error'});
     return next();
+  });
+});
 
+inventory.post('/', (req, res, next) => {
+  //Check if all required data is present
+  if(!(req.body.asset && req.body.model && req.body.serial)){
+    res.status(422).json({error: 'Missing required data'});
+    return next();
+  }
+
+  //Set optional fields
+  const os = req.body.os || null;
+  const location = req.body.location || 'storage';
+  const status = req.body.status || 'working';
+
+  //Insert into database
+  db.result('INSERT INTO inventory (asset, model, serial, os, status, location) VALUES ($1, $2, $3, $4, $5, $6)',
+  [req.body.asset, req.body.model, req.body.serial, os, status, location])
+
+  .then(result => {
+    //Check if insert was successful
+    if(!result.rowCount){
+      res.status(500).json({error: 'Database query failed'});
+      return next();
+    }
+
+    res.json({success: true});
+  })
+
+  .catch(err => {
+    res.status(500).json({error: 'Server Error'});
+    return next();
   });
 });
 
