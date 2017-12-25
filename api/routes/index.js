b4ef9fb6501cb10b84be8248d9140a99f2d5ab6e
treemmett <treemmett@gmail.com>
2017-12-25 15:20:08 -0700

    Adds 404 handling

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,4 +3,10 @@ const routes = require('express').Router();
 //Setup routes
 routes.use('/inventory', require('./inventory'));
 
+//404 handling
+routes.all('*', (req, res, next) => {
+  res.status(404).json({error: 'API endpoint not found'});
+  return next();
+});
+
 module.exports = routes;
